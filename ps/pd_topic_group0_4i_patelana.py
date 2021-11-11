d066db03fe21856077f72543d952c7728699e392
Anandkumar Patel <patelana@umich.edu>
2021-11-11 15:22:41 -0500

    Update pd_topic_group0_4i_patelana.py
    Added Pandas Query notebook

--- ps/pd_topic_group0_4i_patelana.py
+++ ps/pd_topic_group0_4i_patelana.py
@@ -22,54 +22,6 @@
 # Add a bullet for each topic and link to the level 2 title header using 
 # the exact title with spaces replaced by a dash. 
 #
-# + [Topic Title](#Topic-Title) 
+# + [Pandas Query](https://github.com/anandpatel9/Stats507/blob/main/pandas_notes/pd_topic_patelana.ipynb) 
 # + [Topic 2 Title](#Topic-2-Title)
 
-# ## Topic Title
-# Include a title slide with a short title for your content.
-# Write your name in *bold* on your title slide. 
-
-# ## Pandas Topic ##
-# 
-# ## pd. query ##
-# **Name: Anandkumar Patel**
-# ###### Email: patelana@umich.edu
-# ###### Unique ID: patelana
-
-# ### Arguments and Output
-# 
-# **Arguments** 
-# 
-# * expression (expr) 
-# * inplace (default = False) 
-#     * Do you want to operate directly on the dataframe or create new one
-# * kwargs (keyword arguments)
-# 
-# **Returns** 
-# * Dataframe from provided query
-
-# ## Why
-# 
-# * Similar to an SQL query 
-# * Can help you filter data by querying
-# * Returns a subset of the DataFrame
-# * loc and iloc can be used to query either rows or columns
-
-# ## Query Syntax
-# 
-# * yourdataframe.query(expression, inplace = True/False
-
-# ## Code Example
-
-# In[2]:
-
-
-import pandas as pd
-df = pd.DataFrame({'A': range(1, 6),
-                   'B': range(10, 0, -2),
-                   'C C': range(10, 5, -1)})
-print(df)
-
-print('Below is the results of the query')
-
-print(df.query('A > B'))
